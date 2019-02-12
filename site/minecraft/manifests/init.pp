cda7579dd78b7e52b9e9b123be256994e57f5101
sarathchamp <47456989+sarathchamp@users.noreply.github.com>
2019-02-12 17:58:34 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,20 +1,24 @@
 class mincraft{
-file{'/opt/minecraft/':
+file {'/opt/minecraft/':
     ensure:directory,
     }
-file{'/opt/minecraft/minecraft_server.jar':
+file {'/opt/minecraft/minecraft_server.jar':
     ensure=>file,
     source=>'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
     }
 package{'java':
     ensure=>present,
     }
-file{'/opt/minecraft/eula.txt':
+file {'/opt/minecraft/eula.txt':
     ensure=>file,
     content=> 'eula=true',
     }
-file{'/etc/systemd/system/minecraft.service':
+file {'/etc/systemd/system/minecraft.service':
     ensure=>file,
     source=> 'puppet:///modules/minecraft/minecraft.service',
     }    
+service {'minecraft':
+       ensure=>running,
+       enable=>true,
+       }
 }
