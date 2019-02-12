8a093634d6e10b577d700fd957f1a5b0ae89247e
sarathchamp <47456989+sarathchamp@users.noreply.github.com>
2019-02-12 17:02:01 +0530

    Update agent_nodes.pp

--- site/profile/manisfests/agent_nodes.pp
+++ site/profile/manisfests/agent_nodes.pp
@@ -2,4 +2,12 @@ class profile::agent_nodes{
 include dockeragent:
 dockeragentnode::{'web.puppet.vm'}
 dockeragentnode::{'db.puppet.vm'}
+host {'web.puppet.vm':
+    ensure=> present,
+    ip=>'172.18.0.2',
+    }
+    host {'db.puppet.vm':
+    ensure=> present,
+    ip=>'172.18.0.3',
+    }
 }
