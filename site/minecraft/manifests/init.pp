21d5f59dd3508d3d43829099be732cd893062e71
sarathchamp <47456989+sarathchamp@users.noreply.github.com>
2019-02-12 18:16:42 +0530

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,8 @@
-class minecraft{
-$url='https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar'
+class minecraft[
+$url='https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
 $install_dir='/opt/minecraft'
+]
+{
 file {$install_dir:
     ensure:directory,
     }
